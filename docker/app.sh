85f320e11b714af2cb5a887329476b321c756328
Yaroslav <yarikbritov@yandex.ru>
2024-06-02 10:51:36 +0300

    feat: remove useless files and deps

--- docker/app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/busybox
-
-poetry run alembic upgrade head
-
-poetry run uvicorn --reload src.main:testing_company --host=0.0.0.0 --port=9000
--- docker/tg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/busybox
-
-poetry run python3.10 bot/main.py
