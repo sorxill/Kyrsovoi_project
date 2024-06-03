35e99d47306ae02d4488bbe777855c39facd8d10
Yaroslav <yarikbritov@yandex.ru>
2024-06-03 09:37:55 +0300

    docs: add docs to config module

--- config/app_config.py
+++ config/app_config.py
@@ -1,11 +1,15 @@
-"""Main config for all app."""
+"""
+Модуль, отвечающий за получение секретных данных, из переменных окружения.
+"""
 
 import os
 
 from dotenv import load_dotenv
 
+# Загружаем окружение.
 load_dotenv()
 
+# Из объекта environ(окружение) получаем данные переменных.
 DB_NAME = os.environ["DB_NAME"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
